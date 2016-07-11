37885d12b9a877202199f6da138c2a1ef4fb15cd
seongminnpark <seongmi1@andrew.cmu.edu>
2016-07-11 11:11:58 +0900

    Primitive like

---
+++ Volo Like Made Easy/scripts/like.js
@@ -0,0 +1,33 @@
+// like.js
+
+function like() {
+    var cells_to_like = document.getElementsByClassName(cell_class);
+    
+    for (i = 0; i < cells_to_like.length; i++) {
+        var cell_info = get_child_with_class(cells_to_like[i], "c-info");
+
+        if (cell_info != null) {
+            var heart_button = get_child_with_class(cell_info, "like");
+            if (heart_button != null) {
+                heart_button.click();
+            }
+        }
+    }
+}
+
+function get_child_with_class(element, classname) {
+    NodeList.prototype.forEach = Array.prototype.forEach
+
+    var children = element.childNodes;
+    var found = null;
+
+    children.forEach(function(child){
+        if (child.className == classname) {
+            found = child;
+        }
+    });    
+    
+    return found;      
+}
+
+like();
--- Volo Like Made Easy/scripts/popup.js
+++ Volo Like Made Easy/scripts/popup.js
@@ -2,20 +2,79 @@
 
 window.onload = function() {
     init();
-
 };
 
 function init() {
-    var select_all_checkbox = document.getElementById("select-all-checkbox");
+    // Set up onclick for select_all.
+    var select_all_checkbox = document.getElementById("VLME-all");
     select_all_checkbox.onclick = function() {
         toggle_select_all(select_all_checkbox.checked);
+    }
+
+    // Set up onclick for like.
+    var like_button = document.getElementById("VLME-like-button");
+    like_button.onclick = function() {
+        like_button_pressed();
+    }
+
+    // Set up onclick for unlike.
+    var unlike_button = document.getElementById("VLME-unlike-button");
+    unlike_button.onclick = function() {
+        unlike_button_pressed();
     }
 }
 
 function toggle_select_all(previous_value) {
-    var filters = document.getElementsByClassName("filter-checkbox");
+    var filters = document.getElementsByClassName("VLME-filter");
     [].forEach.call(filters, function (filter) {
         filter.checked = previous_value;
     });
 }
 
+function like_button_pressed() {
+    var selected_celltypes = get_selected_celltypes();
+
+    for (i = 0; i < selected_celltypes.length; i++) {
+        like_cell(selected_celltypes[i]);
+    }
+}
+
+function unlike_button_pressed() {
+    var selected_celltypes = get_selected_celltypes();
+
+    for (i = 0; i < selected_celltypes.length; i++) {
+        unlike_cell(selected_celltypes[i]);
+    }
+}
+
+function get_selected_celltypes() {
+    var selected_types = [];
+
+    var filters = document.getElementsByClassName("VLME-filter");
+    [].forEach.call(filters, function (filter) {
+        if (filter.checked) {
+            var trimmed_id = (filter.id).replace('VLME-','');
+            selected_types.push(trimmed_id);
+        }
+    });
+
+    return selected_types;
+}
+
+function like_cell(type_id) {
+    chrome.tabs.executeScript({
+            code: 'var cell_class = "' + type_id + '";'
+        }, function() {
+            chrome.tabs.executeScript({file: 'scripts/like.js'});
+    }); 
+}
+
+function unlike_cell(type_id) {
+    var cell_class = type_id.replace('VLME-','');
+    
+    chrome.tabs.executeScript({
+            code: 'var cell_class = "' + cell_class + '";'
+        }, function() {
+            chrome.tabs.executeScript({file: 'scripts/unlike.js'});
+    });    
+}
